2add640fa6f0bc5139f77d862c9f96f3cfc94473
Natalia Górecka <159615671+njgorecka@users.noreply.github.com>
2026-06-14 23:02:24 +0200

    Add files via upload

---
+++ 05 Wprowadzenie do obsługi błędów i logowania/WeatherAPI_10653_1/Controllers/TestErrorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherAPI_10653.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestErrorsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new Exception("Testowy wyjątek");
+        }
+    }
+}
